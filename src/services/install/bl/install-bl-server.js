0558a3f78744a54b160b0c1e7156c9eaab47d415
Sergey Kukurudzyak <skukurudzyak@gmail.com>
2021-10-18 12:45:58 +0300

    BKNDLSS-26512 Add rt server to pro manager (#2)
    Co-authored-by: sergey.kukurudzyak <sergey@backendless.com>

--- src/services/install/bl/install-bl-server.js
+++ src/services/install/bl/install-bl-server.js
@@ -2,6 +2,9 @@ import { k8sAppsV1Api, k8sCoreV1Api } from '../../k8s/k8s'
 import config from '../../../../config/config.json'
 import blServerK8sConfig from '../../k8s/config/server.json'
 import { installStatus } from '../install-status'
+import { Logger } from '../../../logger'
+
+const logger = Logger('install-bl-server')
 
 export async function installBlServer({ mountPath, version }) {
     installStatus.info('installing bl-server...')
@@ -24,6 +27,7 @@ export async function installBlServer({ mountPath, version }) {
     })
 
     installStatus.info('creating statefulset for bl-server')
+    logger.verbose(`creating workload for bl-server with config: ${JSON.stringify(workload)}`)
     const createStatefulSetResult = await k8sAppsV1Api.createNamespacedStatefulSet(config.k8s.namespace, workload)
     installStatus.info('creating service for bl-server')
     const createServiceResult = await k8sCoreV1Api.createNamespacedService(config.k8s.namespace, blServerK8sConfig.service)
