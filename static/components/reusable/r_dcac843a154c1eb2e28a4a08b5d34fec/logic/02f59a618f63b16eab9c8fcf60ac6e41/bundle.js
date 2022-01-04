8c1d02fa7558529b52c8fcb2da190830995b4544
Roman <66306995+RomanR42@users.noreply.github.com>
2022-01-04 17:07:58 +0200

    BKNDLSS-27244 Create mySQL configuration page for backendless pro

--- static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/02f59a618f63b16eab9c8fcf60ac6e41/bundle.js
+++ static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/02f59a618f63b16eab9c8fcf60ac6e41/bundle.js
@@ -2,7 +2,9 @@ define([], () => ({
   /* content */
   /* handler:onClick */
   async onClick(___arguments) {
-    function addItemToList(l, v) { Array.prototype.push.apply(l, Array.isArray(v) ? v : [v]);return l;}
+    var class2, list;
+
+function addItemToList(l, v) { Array.prototype.push.apply(l, Array.isArray(v) ? v : [v]);return l;}
 
 
   if ((await ( async function (typeSize, component){ return component && component.el ? component.el[`${typeSize}`] : null })('clientHeight', ((function (componentUid){ return ___arguments.context.getComponentByUid(componentUid) })('ca8df858b6abfa78f849814f98cbadeb')))) == 0) {
@@ -12,7 +14,9 @@ define([], () => ({
     addItemToList(((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('02f59a618f63b16eab9c8fcf60ac6e41')), 'aside__TabHeader--rotated');
   } else {
     ((function (componentUid){ return ___arguments.context.getComponentStyleByUid(componentUid) })('ca8df858b6abfa78f849814f98cbadeb'))['maxHeight'] = (String(0) + String('px'));
-    ((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('02f59a618f63b16eab9c8fcf60ac6e41')).pop();
+    while (((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('02f59a618f63b16eab9c8fcf60ac6e41')).length > 2) {
+      ((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('02f59a618f63b16eab9c8fcf60ac6e41')).pop();
+    }
   }
 
   },
--- static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/1fed8d3527f135219fb66d9de15adb04/bundle.js
+++ static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/1fed8d3527f135219fb66d9de15adb04/bundle.js
@@ -9,7 +9,9 @@ define([], () => ({
     ((function (componentUid){ return ___arguments.context.getComponentStyleByUid(componentUid) })('3c48282edad672c3a97414a89b3c15df'))['maxHeight'] = (String((await (async function(element) {
     	return element.el.scrollHeight
     })(((function (componentUid){ return ___arguments.context.getComponentByUid(componentUid) })('3c48282edad672c3a97414a89b3c15df'))))) + String('px'));
-    ((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('1fed8d3527f135219fb66d9de15adb04')).pop();
+    while (((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('1fed8d3527f135219fb66d9de15adb04')).length > 2) {
+      ((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('1fed8d3527f135219fb66d9de15adb04')).pop();
+    }
   } else {
     ((function (componentUid){ return ___arguments.context.getComponentStyleByUid(componentUid) })('3c48282edad672c3a97414a89b3c15df'))['maxHeight'] = (String(0) + String('px'));
     addItemToList(((function (componentUid){ return ___arguments.context.getComponentClassesByUid(componentUid) })('1fed8d3527f135219fb66d9de15adb04')), 'aside__TabHeader--rotated');
