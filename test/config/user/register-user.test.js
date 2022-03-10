05ac4046d3a765dee63be489c041c43eb09a7090
Sergii Kukurudziak <sergey@backendless.com>
2022-03-10 14:30:22 +0200

    removed ';'

--- test/config/user/register-user.test.js
+++ test/config/user/register-user.test.js
@@ -1,14 +1,14 @@
-import {userService} from "../../../src/services/user";
-import {JsonDb} from "../../../src/json-db";
-import {UserError} from "../../../src/error/user-error";
+import {userService} from "../../../src/services/user"
+import {JsonDb} from "../../../src/json-db"
+import {UserError} from "../../../src/error/user-error"
 
 describe('user registration', () => {
     beforeEach(() => {
-        userService._jsonDb = new JsonDb({ dbPath: `${__dirname}/test-user.json` })
+        userService._jsonDb = new JsonDb({dbPath: `${__dirname}/test-user.json`})
         userService._jsonDb.getDbSync().delete('/')
     })
     test('should register user', async () => {
-        const userToCreate = {login: 'test', password: 'test'};
+        const userToCreate = {login: 'test', password: 'test'}
         const user = await userService.register(userToCreate)
         expect(typeof user.id).toBe('string')
         expect(typeof user.created).toBe('number')
@@ -18,7 +18,7 @@ describe('user registration', () => {
 
     test('should throw duplicate error', async () => {
 
-        const userToCreate = {login: 'testexists', password: 'test'};
+        const userToCreate = {login: 'testexists', password: 'test'}
         await userService.register(userToCreate)
         const userFunction = async () => await userService.register(userToCreate)
         await expect(userFunction).rejects.toThrow(UserError.DuplicateUserError)
