a6a81247078cdfd59f35415c04809914e179c305
Sergii Kukurudziak <sergey@backendless.com>
2022-03-11 14:33:21 +0200

    BKNDLSS-27905 Add route to get users

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -8,8 +8,13 @@ const _maxCookieAge = 1000*3600*24*60
 router.post('/register', handler(({ body }) => {
     return userService.register(body)
 }))
+
 router.post('/login', handler(({ res, body }) => {
     const token = userService.login(body)
     res.cookie('auth-token', token, { maxAge: _maxCookieAge })
     return token
+}))
+
+router.get('/', handler(() => {
+    return userService.get()
 }))
