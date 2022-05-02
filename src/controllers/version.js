8d87ed011cc74e9a4c4494a191f0d2011ac51062
Sergii Kukurudziak <sergey@backendless.com>
2022-05-02 16:08:35 +0300

    BKNDLSS-27879 Add route to get and filter backendless version

---
+++ src/controllers/version.js
@@ -0,0 +1,7 @@
+import { Router } from '../utils/router'
+
+const proxy = require('express-http-proxy')
+
+export const router = new Router()
+
+router.use('/docker-hub', proxy('https://hub.docker.com'))
