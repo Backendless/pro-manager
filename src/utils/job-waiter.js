035318ca5c882a9b20752b6bac3bebe06653c2dc
Sergii Kukurudziak <sergey@backendless.com>
2022-09-07 18:28:05 +0300

    added exception handler in waiter

--- src/utils/job-waiter.js
+++ src/utils/job-waiter.js
@@ -6,16 +6,33 @@ import { Logger } from '../logger'
 const logger = Logger('job-waiter')
 
 export async function waitForJobToComplete(jobName, jobNotCompleteCallback){
-    const status = await blJobStatus(jobName)
+    try {
+        const status = await blJobStatus(jobName)
 
-    if (status.status === Status.complete)
-        return
+        if (status.status === Status.complete)
+            return
+    } catch (e) {
+        logger.error(`Error during status check. Error: ${e}, \nObject: ${JSON.stringify(e, getCircularReplacer())}`)
+    }
 
-    if(jobNotCompleteCallback !== undefined) {
+    if (jobNotCompleteCallback !== undefined) {
         jobNotCompleteCallback()
     }
 
     logger.info('job is not completed, waiting for 1 second to recheck')
     await wait(1000)
     await waitForJobToComplete(jobName, jobNotCompleteCallback)
+}
+
+function getCircularReplacer() {
+    const seen = new WeakSet()
+    return (key, value) => {
+        if (typeof value === 'object' && value !== null) {
+            if (seen.has(value)) {
+                return '[circular]'
+            }
+            seen.add(value)
+        }
+        return value
+    }
 }
