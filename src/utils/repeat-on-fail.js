f247df96edb9090bd158801b7719033611630e87
Sergii Kukurudziak <sergey@backendless.com>
2022-09-08 15:48:07 +0300

    BKNDLSS-29585 Install pro failed during first setup

--- src/utils/repeat-on-fail.js
+++ src/utils/repeat-on-fail.js
@@ -9,15 +9,13 @@ export async function repeatOnFail(func, repeatTimes, waitInMillis) {
         return await func()
     } catch (e) {
         if (repeatTimes > 0) {
-            logger.error(`Failed to execute function '${func.name}', wait time is ${waitInMillis}, 
-                repeat times ${repeatTimes}. 
-                The error is: ${JSON.stringify(e, circularReplacer())}`)
+            logger.error(`Failed to execute function '${func.name}', wait time is ${waitInMillis}, repeat times ${repeatTimes}. The error is: ${JSON.stringify(e, circularReplacer())}`)
           
             if (waitInMillis) {
                 await wait(waitInMillis)
             }
 
-            return await repeatTimes(func, repeatTimes - 1, waitInMillis)
+            return await repeatOnFail(func, repeatTimes - 1, waitInMillis)
         } else {
             throw e
         }
