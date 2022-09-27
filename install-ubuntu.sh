6edfbeb955ae74a210a0d309903b95eea8802f9b
Sergii Kukurudziak <sergey@backendless.com>
2022-09-27 15:58:22 +0300

    fixed install ubuntu script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
-    && source ~/.bashrc \
-    && nvm install v14.18.2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install v14.18.2
 
 curl -sfL https://get.k3s.io | sh - \
      && mkdir ~/.kube \
-     && echo "export KUBECONFIG=~/.kube/config" >> ~/.bashrc \
-     && source ~/.bashrc \
-     && sudo k3s kubectl config view --raw > "$KUBECONFIG"
+     && echo "export KUBECONFIG=~/.kube/config" >> ~/.bashrc
+
+export KUBECONFIG=~/.kube/config
+sudo k3s kubectl config view --raw > "$KUBECONFIG"
 
 git clone https://github.com/Backendless/pro-manager.git
 
