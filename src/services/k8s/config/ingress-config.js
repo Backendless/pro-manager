7bf2de2153e6eb3ee73fe0d3e2b48818209a37c0
Sergii Kukurudziak <sergey@backendless.com>
2022-12-29 16:32:40 +0200

    removed traefik

--- src/services/k8s/config/ingress-config.js
+++ src/services/k8s/config/ingress-config.js
@@ -14,6 +14,7 @@ class IngressConfig {
                     }
                 }
             },
+            'ingressClassName': 'nginx',
             'rules': [
                 {
                     'host': 'api.backendless.local',
