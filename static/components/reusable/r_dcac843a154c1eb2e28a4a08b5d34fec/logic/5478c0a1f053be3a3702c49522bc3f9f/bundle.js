39d6d986acb90b909fb3272a724770abdaf8c8a2
Sergey Kukurudzyak <skukurudzyak@gmail.com>
2023-05-09 12:39:42 +0300

    Revert "Correct the layout of the domains/Add mail setting section in Pro manager (#89)" (#90)
    Reverted because of BKNDLSS-32368The application is failed to run
    
    The following tickets will be reverted
    BKNDLSS-31003
    BKNDLSS-32085
    BKNDLSS-32129
    BKNDLSS-31351
    BKNDLSS-30931
    BKNDLSS-31837

--- static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/5478c0a1f053be3a3702c49522bc3f9f/bundle.js
+++ static/components/reusable/r_dcac843a154c1eb2e28a4a08b5d34fec/logic/5478c0a1f053be3a3702c49522bc3f9f/bundle.js
@@ -30,32 +30,12 @@ define([], () => ({
   },
   /* handler:onClick */
   /* handler:onClassListAssignment */
-  ['onClassListAssignment'](___arguments) {
+  onClassListAssignment(___arguments) {
     var classList;
-
-function getObjectProperty(object, propPath) {
-  if (typeof propPath !== 'string' || object[propPath] !== undefined) {
-    return object[propPath]
-  }
-
-  const propsNamesList = propPath.split('.')
-
-  let result = object
-
-  for (let i = 0; i < propsNamesList.length; i++) {
-    if (!result || result[propsNamesList[i]] === undefined) {
-      return
-    }
-
-    result = result[propsNamesList[i]]
-  }
-
-  return result
-}
 
 
   classList = ['aside__TabContentItem'];
-  if ((getObjectProperty(___arguments.context.pageData, 'pageSettings')) == 'userManagement') {
+  if ((___arguments.context.pageData['pageSettings']) == 'userManagement') {
     classList = ['aside__TabContentItem', 'aside__TabContentItem--active'];
   }
 
