8b7e6eb190d7d1a33237758d27300f2d4479800b
Sergey Kukurudzyak <skukurudzyak@gmail.com>
2023-05-19 14:15:47 +0300

    BKNDLSS-32504 Add route to list versions

--- src/services/system/version-service.js
+++ src/services/system/version-service.js
@@ -1,10 +1,28 @@
 import simpleGit from 'simple-git'
+import { Logger } from '../../logger'
+
+const logger = Logger('version-service')
+
+function convertTagToVersion(tag) {
+    const tagParts = tag.split('_')
+    return tagParts.length > 1 ? tagParts[1] : tagParts[0]
+}
 
 class VersionService {
     async getCurrent() {
         const tag = await simpleGit().raw(['describe', '--tags'])
-        const tagParts = tag.split('_')
-        return tagParts.length > 1 ? tagParts[1] : tagParts[0]
+        logger.info(`current tag is [${tag}]`)
+        return convertTagToVersion(tag)
+    }
+
+    async list() {
+        const fetchResult = await simpleGit().fetch(['--tags'])
+        logger.info(`fetch result is [${JSON.stringify(fetchResult)}]`)
+        const tags = await simpleGit().tags(['-l', 'release_*'])
+        logger.info(`available release tags is [${JSON.stringify(tags)}]`)
+        tags.all = tags.all.map(tag => convertTagToVersion(tag))
+        tags.latest = convertTagToVersion(tags.latest)
+        return tags
     }
 }
 
