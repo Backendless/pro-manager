2ec778deebc08e03c01a6843e20006d418c5e9e7
Sergey Kukurudzyak <skukurudzyak@gmail.com>
2023-05-19 13:46:50 +0300

    version should be a part after '_'

--- src/services/system/version-service.js
+++ src/services/system/version-service.js
@@ -2,7 +2,9 @@ import simpleGit from 'simple-git'
 
 class VersionService {
     async getCurrent() {
-        return simpleGit().raw(['describe', '--tags'])
+        const tag = await simpleGit().raw(['describe', '--tags'])
+        const tagParts = tag.split('_')
+        return tagParts.length > 1 ? tagParts[1] : tagParts[0]
     }
 }
 
