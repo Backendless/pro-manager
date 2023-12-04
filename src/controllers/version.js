c3cfae8cee45fd16a51ca9e1e96725d08f88cf1f
Sergey Kukurudzyak <skukurudzyak@gmail.com>
2023-12-04 17:44:42 +0200

    BKNDLSS-34653 [PRO]The request entity exceeds the maximum allowed

--- src/controllers/version.js
+++ src/controllers/version.js
@@ -4,4 +4,4 @@ const proxy = require('express-http-proxy')
 
 export const router = new Router()
 
-router.use('/docker-hub', proxy('https://hub.docker.com'))
+router.use('/docker-hub', proxy('https://hub.docker.com', { parseReqBody: false }))
