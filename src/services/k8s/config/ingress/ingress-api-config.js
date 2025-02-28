e293ded5bf5b06c0374cd76eebfa4bc562b1a58d
Andriy Konoz <andriy.konoz@themidnightcoders.com>
2025-02-28 15:28:28 +0200

    Corrected new rule to match rules from other environments

--- src/services/k8s/config/ingress/ingress-api-config.js
+++ src/services/k8s/config/ingress/ingress-api-config.js
@@ -67,7 +67,7 @@ export class IngressApiConfig {
                                 }
                             },
                             {
-                                'path': '/automation/',
+                                'path': '/automation',
                                 'pathType': 'Prefix',
                                 'backend': {
                                     'service': {
