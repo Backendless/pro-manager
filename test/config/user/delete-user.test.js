eb47dafe542f51d74980a284c46629028f36c312
Sergey Androsov <sergey.androsov@backendlessmail.com>
2025-07-01 10:46:50 +0300

    PRO-127 - Can't start FlowRunner in ProManager event if license allows it

--- test/config/user/delete-user.test.js
+++ test/config/user/delete-user.test.js
@@ -2,7 +2,6 @@ import {userService} from "../../../src/services/user"
 import {JsonDb} from "../../../src/json-db"
 import {UserError} from "../../../src/error/user-error"
 import {userConfig} from "../../../src/config/user-config"
-import jwt from 'jsonwebtoken'
 
 describe('user remove', () => {
     beforeEach(() => {
@@ -13,13 +12,28 @@ describe('user remove', () => {
 
     test('should remove existed user', async () => {
         const userToCreate = {login: 'test', password: 'test'}
+        const userToCreate2 = {login: 'test2', password: 'test2'}
+        await userService.register(userToCreate2)
         const user = await userService.register(userToCreate)
 
         await userService.remove(user.id)
-
     })
 
     test('should remove NOT existed user', async () => {
+        const userToCreate = {login: 'test', password: 'test'}
+        const userToCreate2 = {login: 'test2', password: 'test2'}
+        await userService.register(userToCreate)
+        await userService.register(userToCreate2)
+
         await userService.remove('not existed user')
+    })
+
+    test('should throw FailToRemoveLastUserError when trying to remove user and less than 2 users exist', async () => {
+        const userToCreate = {login: 'test', password: 'test'}
+        const user = await userService.register(userToCreate)
+
+        await expect(userService.remove(user.id))
+          .rejects
+          .toThrow(UserError.FailToRemoveLastUserError)
     })
 })
